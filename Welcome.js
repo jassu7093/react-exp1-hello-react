1440090eb4a6cc30c5008fd7950f7456f79f92ec
jassu7093 <155291358+jassu7093@users.noreply.github.com>
2025-11-07 19:55:00 +0530

    Add files via upload

---
+++ Welcome.js
@@ -0,0 +1,13 @@
+// src/components/Welcome.js
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>👋 Welcome to React Experiment 1</h1>
+      <p>This is a simple functional component in React.</p>
+    </div>
+  );
+};
+
+export default Welcome;
